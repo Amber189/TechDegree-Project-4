c1673528eb8a6561e4366a3869d389184e5e4618
Amber <32441978+Amber189@users.noreply.github.com>
2018-04-29 15:23:12 -0500

    final edits
    javascript code is functioning for search field!

--- js/app.js
+++ js/app.js
@@ -11,95 +11,20 @@ let input = document.getElementById('search');
 let search;
 let image = document.querySelectorAll('a[data-title]');
 let caption;
-                // let match = document.querySelectorAll('li a');
-                // match= match.getAttribute('data-title');
-
-                // let li = document.querySelector("li");
-                // let matches = li.querySelectorAll("a[data-title]");
+                
 
     input.addEventListener('keyup', function(event){
 
       search = input.value.toLowerCase();
-      caption = image.getAttribute("data-title");
 
-      for (let i = 0; i < caption.length; i++ ){
-        if (caption[i].getAttribute("data-title").toLowerCase().includes(search)){
-          console.log(caption[i]);
-        //   caption[i].style.display='';
-        // } else {
-        //   caption[i].style.display = 'hidden';
+
+      for (let i = 0; i < image.length; i++ ){
+        caption = image[i].getAttribute("data-title");
+        if (caption.toLowerCase().includes(search)){
+          // console.log(caption);
+          image[i].style.display='';
+        } else {
+          image[i].style.display = 'none';
         }
       }
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //declare variables
-//   const input = document.getElementById('search');
-//   const filter = input.value.toUpperCase();
-//   const ul = document.getElementById("myUL");
-//   const li = ul.getElementsByTagName ('li');
-//
-// function searchImages () {
-//  // Loop through all list items, and hide those who don't match the search query
-//   for (i = 0; i < li.length; i++) {
-//       let a = li [i] .getElementsByTagName("a")[0];
-//     if (a.innerHTML.toUpperCase().indexOf(filter) > -1){
-//       li[i].style.display = "";
-//     } else{
-//         li[i].style.display = "none";
-//       }
-//     }
-//     search.addEventListener(click, searchImages, )
-
-
-
-
-//
-// let input = document.getElementById('search');
-//
-//   input.addEventListener('keyup', (event) => {
-//     input.value = input.value.toLowerCase();
-//
-//      let a = document.querySelectorAll('a');
-//       let cap = a.getAttribute("data-title");
-//     for (let i = 0; i < a.length; i++ ){
-//        // if (input.value.toLowerCase().includes("data-title") == true){
-//          if(cap[i].toLowerCase().includes(input) > -1){
-//            cap[i].style.display = '';
-//       } else {
-//         cap[i].style.display='hidden';
-//       };
-//     };
-// });
-// //
-
-
-
-// $('#input').keyup(function(){
-
-    // let userSearch = $('#input').val();
-    // userSearch = userSearch.toLowerCase();
-
-    // let match = document.querySelectorAll('a[data-title]');
-
-    // for (i = 0; i < match.length; i++) {
-
-    // if (match[i].getAttribute('data-title').toLowerCase().includes(userSearch)) {
-//     match[i].style.display='inline';
-//     } else {
-//     match[i].style.display='none';
-//     }
-//   }
-// });
